f748ca782fd9b17710281445aefbb697e5026680
Adrien Torris <adrientorris@gmail.com>
2017-03-10 23:00:49 +0100

    Core migration fom .NET Framework class libraries to .NET Standard class libraries

---
+++ src/Tablator.BusinessModel/Hierarchy.cs
@@ -0,0 +1,68 @@
+﻿namespace Tablator.BusinessModel
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class HierarchyModel
+    {
+        public Guid Id { get; }
+
+        public string Name { get; }
+
+        public string Description { get; }
+
+        public string Picture { get; }
+
+        public int Position { get; }
+
+        public Guid? ParentId { get; }
+
+        public HierarchyModel Ascendance { get; }
+
+        public HierarchyCollectionModel Descendance { get; }
+
+        public HierarchyTabReferenceCollectionModel Tablatures { get; }
+
+        public bool Root => ParentId == null || ParentId.Value == Guid.Empty ? true : false;
+
+        public bool HasChildren => Descendance == null || Descendance.Count == 0 ? true : false;
+
+        public HierarchyModel(Guid id, string name, string desc, string pic, int posi)
+        {
+            Id = id;
+            Name = name;
+            Description = desc;
+            Picture = pic;
+            Position = posi;
+
+            Descendance = new HierarchyCollectionModel();
+        }
+    }
+
+    public class HierarchyCollectionModel : List<HierarchyModel>
+    {
+        public HierarchyCollectionModel()
+        { }
+
+        public HierarchyCollectionModel GetRootLevel()
+        {
+            HierarchyCollectionModel ret = new HierarchyCollectionModel();
+            ret.AddRange(this.Where(x => !x.ParentId.HasValue));
+            return ret;
+        }
+    }
+
+    public class HierarchyTabReferenceModel
+    {
+        public string Name { get; }
+        public string UrlTitle { get; }
+        public int Position { get; }
+    }
+
+    public class HierarchyTabReferenceCollectionModel : List<HierarchyTabReferenceModel>
+    {
+        public HierarchyTabReferenceCollectionModel()
+        { }
+    }
+}
---
+++ src/Tablator.BusinessModel/Tablature/BasePropertyItemModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tablator.BusinessModel.Tablature
+{
+    public abstract class BasePropertyItemModel
+    {
+        public int Code { get; set; }
+
+        public string Value { get; set; }
+
+        public BasePropertyItemModel(int code, string val)
+        {
+            Code = code;
+            Value = val;
+        }
+    }
+}
